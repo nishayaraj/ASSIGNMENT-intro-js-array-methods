63dda7e7130947fc2680ac70e54836638624e41b
nishaya raj <nishayaraj@gmail.com>
2022-04-27 20:40:03 -0500

    aray method trials

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,11 @@ import { renderToDom } from "../utils/renderToDom.js";
 // Reusable function to get the cards on the DOM
 // .forEach()
 const renderCards = (array) => {
-  let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+  let refStuff = "";
+
+  array.forEach((item) => {
+    refStuff += card(item);
+  });
   renderToDom("#cards", refStuff);
 }
 
@@ -14,35 +18,48 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   //console.log('event.target.id');
+   //destructuring*******************
+   const [,id] = event.target.id.split("--");
+      const index = referenceList.findIndex(taco => taco.id === Number(id))
+      referenceList[index].inCart = ! referenceList[index].inCart;
+      cartTotal();
+      renderCards(referenceList);
   }
 }
 
 // SEARCH
 // .filter()
 const search = (event) => {
-  const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
+  
+const userInput = event.target.value.toLowerCase();
+const searchResult = referenceList.filter(taco => 
+taco.title.toLocaleLowerCase().includes(userInput) ||
+taco.author.toLocaleLowerCase().includes(userInput) ||
+taco.description.toLocaleLowerCase().includes(userInput) 
+);
+ renderCards(searchResult);
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    console.log('FREE')
+    const free = referenceList.filter(item => item.price <= 0);
+    renderCards(free);
   }
   if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+    const wishList = referenceList.filter(taco => taco.inCart);
+    renderCards(wishList);
   }
   if(event.target.id.includes('books')) {
-    console.log('books!')
+    const books = referenceList.filter(item => item.type.toLowerCase()=== 'book');
+    renderCards(books);
   }
   if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+   renderCards(referenceList);
   }
-  if(event.target.id.includes('cartList')) {
-    cartList();
-  }
+  
   if(event.target.id.includes('productList')) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
     <thead>
@@ -54,8 +71,8 @@ const buttonFilter = (event) => {
     </thead>
     <tbody>
     `;
-    
-    productList().forEach(item => {
+    //look up localcompare()
+    productList().sort((a,b) => a.type.localeCompare(b.type)).forEach(item => {
       table += tableRow(item);
     });
 
@@ -69,14 +86,27 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const cart = referenceList.filter(taco => taco.inCart);
+  const total = cart.reduce((taco1,taco2) => taco1 + taco2.price, 0);
+  const free = cart.some(taco => taco.orice <=0);
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
+
+
+  //check if this code is done
+  if (free) {
+    document.querySelector('#includes-free').innerHTML = "includes free items"
+  }else{
+    document.querySelector("#includes-free.innerHTML")
+  }
 }
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
+  return referenceList.map(item => ({
+    title:item.title, 
+    price:item.price, 
+    type:item.type}))
 }
 
 
